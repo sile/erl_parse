38de305227b5529245e29a6ab950e95cc87413b8
Takeru Ohta <phjgt308@gmail.com>
2017-06-18 16:52:49 +0900

    Make parse command executable

--- examples/parse.rs
+++ examples/parse.rs
@@ -1,25 +1,33 @@
 extern crate clap;
+extern crate erl_pp;
 extern crate erl_parse;
-// #[macro_use]
+extern crate erl_tokenize;
+#[macro_use]
 extern crate trackable;
 
-// use std::fs::File;
-// use std::io::Read;
-// use clap::{App, Arg};
-// use erl_parse::Parser;
-// use trackable::error::{Failed, ErrorKindExt};
+use std::fs::File;
+use std::io::Read;
+use clap::{App, Arg};
+use erl_pp::Preprocessor;
+use erl_parse::{TokenReader, Tokens, Parser};
+use erl_parse::cst::ModuleDecl;
+use erl_tokenize::Lexer;
+use trackable::error::{Failed, ErrorKindExt};
 
 fn main() {
-    // let matches = App::new("parse")
-    //     .arg(Arg::with_name("ERLANG_FILE").index(1).required(true))
-    //     .get_matches();
-    // let erlang_file = matches.value_of("ERLANG_FILE").unwrap();
+    let matches = App::new("parse")
+        .arg(Arg::with_name("ERLANG_FILE").index(1).required(true))
+        .get_matches();
+    let erlang_file = matches.value_of("ERLANG_FILE").unwrap();
 
-    // let mut file = track_try_unwrap!(File::open(erlang_file).map_err(|e| Failed.cause(e)));
-    // let mut text = String::new();
-    // track_try_unwrap!(file.read_to_string(&mut text).map_err(|e| Failed.cause(e)));
+    let mut file = track_try_unwrap!(File::open(erlang_file).map_err(|e| Failed.cause(e)));
+    let mut text = String::new();
+    track_try_unwrap!(file.read_to_string(&mut text).map_err(|e| Failed.cause(e)));
 
-    // let parser = track_try_unwrap!(Parser::from_text(&text));
-    // let module = track_try_unwrap!(parser.parse_module());
-    // println!("{:?}", module);
+    let mut parser = Parser::new(TokenReader::new(
+        Tokens::new(Preprocessor::new(Lexer::new(text))),
+    ));
+
+    let module: ModuleDecl = track_try_unwrap!(parser.parse());
+    println!("{:?}", module);
 }
